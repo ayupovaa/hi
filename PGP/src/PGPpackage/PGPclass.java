6f85250b4c3f600d0d49d3e64dbbb03866a3eb7c
ayupo <ayupo@L-laptop>
2019-02-25 11:56:41 +0500

    lalala

--- PGP/src/PGPpackage/PGPclass.java
+++ PGP/src/PGPpackage/PGPclass.java
@@ -1,56 +1,36 @@
-package PGPpackage;//Папка где находиться код программы
+package PGPpackage;
+import java.io.IOException; // импортируемые библиотеки
+import java.util.Scanner;
 
-//Подключения библиотек
-import java.io.IOException; 
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
+public class PGPclass {  //открытый класс PGPclass
+	 
+	// метод main – точка начала выполнения программы
+	public static void main(String[] args) throws IOException, Exception 
+	{ 
+	  Scanner sc = new Scanner(System.in); // класс для ввода информации
+	
+        String Ley = ""; 
+    
+		
+        try // проверка выполнения кода
+        {        	
+		System.out.print("Введите текст: "); 
+		Ley = sc.nextLine() ; 
+        }
+       
+        
+        catch(Exception e) // в случае исключения
+		{   
+            System.out.println("Incorrect format!");
+    		sc.close();      	
+   		}
 
-public class PGPclass { 
-public static void main(String[] args) throws IOException { 
-	
-	JPanel windowContent= new JPanel(); // Создаём форму(панель)
-	
-	 // Создаём компоненты в памяти
-	 JLabel label = new JLabel("Введите текст:");
-	 JTextArea area = new JTextArea(20,50);
-	 JLabel otvet = new JLabel("");
-	 JButton go = new JButton("Подсчет символов");
-	
-	 go.addActionListener(new ActionListener() { //Создаем обработчик события
-         public void actionPerformed(ActionEvent e) { //вызов класса
-        	 
-        	 //объявление переменных
-        	 String str; 
-        	 int Ley = 0, Probel = 0, Y = 0;
-        	 
-        	 str = area.getText(); //присвоение переменной
-        	 
-        	 	if(str.length() != 0){ //если текстовое поле не = 0
-		        	 for(int i=0;i<str.length();i++){ 	//подсчет всех символов
-		        			 Ley++;  
-				        		if (str.charAt(i) == '\n'){//подсчет переносов строк
-				        				 Probel++;}
-				        		if (str.charAt(i) == ' '){//подсчет пробелов
-				        				 Probel++;}} 
-		        	 
-				        		 Y=Ley-Probel;//подсчет символов без пробелов
-				        		 
-				        		 otvet.setText("В тексте "+Y+" символов"); //вывод количества символов
-				        	 	}
-        	 		else{ //если в текстовом поле пусто выводим предупреждение
-        	 		JOptionPane.showMessageDialog(null, "Введите текст","Предупреждение",JOptionPane.WARNING_MESSAGE);
-       }}});
-	 
-	
-			 JFrame frame = new JFrame("Подсчет символов");// создаём окно
-			 frame.setContentPane(windowContent);//задаём для него форму
-			 frame.setSize(600, 500);// задаём и размер
-			 frame.setVisible(true); //делаем фрейм видимым
-	 
-	// Добавляем компоненты на панель
-		windowContent.add(label);
-		 windowContent.add(area);
-		 windowContent.add(otvet);
-		 windowContent.add(go);
-	   }}
+     // создание экземпляра класса PGPs
+        PGPs s = new PGPs(Ley);
+		
+		System.out.println("--------------------------------");
+		System.out.println("Количество символов в тексте: " + s.Sum);
+
+    	sc.close(); // освобождение ресурсов
+}
+}
